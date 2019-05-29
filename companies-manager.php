28fd9f66651e0ba77a07a444d263bfe059c20b6c
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-05-29 15:06:37 +0300

    Add new company command

--- companies-manager.php
+++ companies-manager.php
@@ -1,7 +1,7 @@
 <?php
 $data_json = file_get_contents('data.json'); // get data file
-if (!empty($data_json)){
-    $data = json_decode($data_json, true); // convert json string to PHP data array
+$data = json_decode($data_json, true); // convert json string to PHP data array
+if (!empty($data)){
     echo "----COMPANIES LIST----\n";
     echo "id|name|registration_code|email|phone|comment|\n";
     function printOneColumn($str){ // print one column
@@ -19,5 +19,23 @@ if (!empty($data_json)){
 }
 else{
     echo "Companies list is empty.";
+}
+if ($argc > 1){
+    $command = $argv[1];
+    switch($command){
+        case 'add': // add new company command
+            $company_id = $argv[1];
+            $company_name = $argv[2];
+            $company_registration_code = $argv[3];
+            $comapny_email = $argv[4];
+            $company_phone = $argv[5];
+            $company_comment = $argv[6];
+        break;
+
+        default: break;
+    }
+}
+else{
+    // print help manual text (in the future I will write that)
 }
 ?>
