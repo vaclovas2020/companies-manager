023d561b3105ff18823d0d6bfbfdb3fc0986d587
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-05-29 15:18:07 +0300

    Added 'add' command arguments check

--- companies-manager.php
+++ companies-manager.php
@@ -1,4 +1,5 @@
 <?php
+require_once('function.error_messages.php'); // error mesages print module
 $data_json = file_get_contents('data.json'); // get data file
 $data = json_decode($data_json, true); // convert json string to PHP data array
 if (!empty($data)){
@@ -24,12 +25,15 @@ if ($argc > 1){
     $command = $argv[1];
     switch($command){
         case 'add': // add new company command
+        if ($argc == 7){
             $company_id = $argv[1];
             $company_name = $argv[2];
             $company_registration_code = $argv[3];
             $comapny_email = $argv[4];
             $company_phone = $argv[5];
             $company_comment = $argv[6];
+        }
+        else require_more_arguments_error();
         break;
 
         default: break;
