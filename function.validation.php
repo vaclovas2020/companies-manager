d75b7fee98698dace78c91438fa5e7b4cf77a455
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-05-29 16:34:05 +0300

    Added function keyword in file function.validation.php

--- function.validation.php
+++ function.validation.php
@@ -1,8 +1,8 @@
 <?php 
-isNumber($str){
+function isNumber($str){
     return (preg_match('/^[0-9]+$/', $str) == 1)?true:false;
 }
-isPhoneNumber($str){
+function isPhoneNumber($str){
     return (preg_match('/(^[+]{1}[0-9]{11}$)|(^[0-9]{9}$)/', $str) == 1)?true:false; // 9 numbers without + or 11 numbers with +
 }
 ?>
