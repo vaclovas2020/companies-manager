60f57974a6c616d6b72c5092dbc8664e92214fca
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-05-29 14:46:23 +0300

    Fixed small issues with companies list table

--- companies-manager.php
+++ companies-manager.php
@@ -3,7 +3,7 @@ $data_json = file_get_contents('data.json'); // get data file
 if (!empty($data_json)){
     $data = json_decode($data_json, true); // convert json string to PHP data array
     echo "----COMPANIES LIST----\n";
-    echo "id|name|registration_code|email|phone|comment\n";
+    echo "id|name|registration_code|email|phone|comment|\n";
     function printOneColumn($str){ // print one column
         echo $str."|";
     }
@@ -14,7 +14,7 @@ if (!empty($data_json)){
         printOneColumn($company['email']);
         printOneColumn($company['phone']);
         printOneColumn($company['comment']);
-        echo '\n'; // add new line
+        echo "\n"; // add new line
     }
 }
 else{
