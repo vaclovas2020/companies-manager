def57a2cd4d7a3df7094403274d5194b2d0556d8
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-05-29 14:39:21 +0300

    Print companies list

--- companies-manager.php
+++ companies-manager.php
@@ -1,4 +1,23 @@
 <?php
-$data_json = file_get_contents('data.json');
-
+$data_json = file_get_contents('data.json'); // get data file
+if (!empty($data_json)){
+    $data = json_decode($data_json, true); // convert json string to PHP data array
+    echo "----COMPANIES LIST----";
+    echo "id|name|registration_code|email|phone|comment";
+    function printOneColumn($str){ // print one column
+        echo $str."|";
+    }
+    foreach ($data as $company){ // print each company data
+        printOneColumn($company['id']);
+        printOneColumn($company['name']);
+        printOneColumn($company['registration_code']);
+        printOneColumn($company['email']);
+        printOneColumn($company['phone']);
+        printOneColumn($company['comment']);
+        echo '\n'; // add new line
+    }
+}
+else{
+    echo "Companies list is empty.";
+}
 ?>
